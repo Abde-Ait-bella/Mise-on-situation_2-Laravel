6059d9969587d4a1467d396c7488789c41d7c6f7
Abdessamad Ait-bella <abdessamadaitbella1998@gmail.com>
2025-03-11 11:07:36 +0000

    mise on situation 3 laravel

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,4 @@ Route::get('/', function () {
     return view('Home');
 });
 
-Route::post('login', [AuthController::class, 'login'])->middleware();
+Route::post('login', [AuthController::class, 'login']);
